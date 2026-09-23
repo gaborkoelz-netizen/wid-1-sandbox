c767a4d75398230cdbb019902103de6696c9977a
gaborkoelz-netizen <gabor.koelz@gmail.com>
2026-09-23 15:13:46 +0200

    FlexBox

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import "./style.css"
 
 export default function App() {
   return (
-    <div>
+    <div id="elter">
 
-
-      <div id="eins" className="gruppe1" style={{backgroundColor: "white", color: "blue"}}>Hallo Welt 1</div>
-      <div>Hallo Welt 2</div>
-
-      <span className="gruppe1">Hallo Mars 1</span>
-      <span>Hallo Mars 2</span>
-
-
+      <div className="kind">Kind</div>
+      <div className="kind">Kind</div>
+      <div className="kind">Kind</div>
 
 
 
